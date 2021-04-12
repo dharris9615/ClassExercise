01cbb403f1bcceb91bd62023e15a3bf0d10e1722
David Harris <davlsu14@gmail.com>
2021-04-12 14:11:12 -0500

    completed exercise 1

---
+++ ClassExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car carOne = new Car();
+            carOne.Make = "Ford";
+            carOne.Model = "F-150";
+            carOne.Year = 2014;
+
+            var audi = new Car()
+            {
+                Make = "audi",
+                Model = "R8",
+                Year = 2017
+
+            };
+            var carList = new List<Car>() { carOne, audi };
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make}, {vehicle.Model}, {vehicle.Year}");
+            } 
+        }
+    }
+}
